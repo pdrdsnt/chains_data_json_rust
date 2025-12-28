7bec297d9706aea71f19f4c057d8856d33b05c35
pedro de santi <pdrodsnt@gmail.com>
2025-12-28 12:52:49 -0300

    clean test

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,17 @@ pub mod chains;
 pub fn test() {
     let default_chains = chains::ChainsJsonInput::default();
     //println!("chains json data decoded: {:?}", default_chains);
+    for c in default_chains.chains {
+        let (p, t, d) = (c.1.pools.len(), c.1.tokens.len(), c.1.dexes.len());
+        println!("chain {:?} lodaded ", c.0);
+        println!("-----------------------------");
+
+        println!("with {} pools ", p);
+        println!("with {} dexes ", d);
+        println!("with {} tokens ", t);
+
+        println!("==============================");
+
+        println!("");
+    }
 }
